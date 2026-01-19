2c1c401f351032a2366bbe66419706be4e0e64e1
xenojoy <joydebnath747@gmail.com>
2026-01-19 19:38:18 +0600

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,13 @@ io.on("connection", (socket) => {
   });
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+
